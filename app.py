74a8568b17c244f2d17c162c03e98bb2b43fba35
ShihWen <unhuman_7932@hotmail.com>
2022-07-29 15:06:55 +0800

    setup connection for database

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Api
+
+from config import config
+from db import db
+
+
+params = config()
+DB_FULL_URL = f"postgresql+psycopg2://{params['user']}:{params['password']}@{params['host']}/{params['database']}"
+
+
+app = Flask(__name__)
+app.config['SQL_ALCHEMY_DATABASE_URI'] = DB_FULL_URL
+app.config['SQL_ALCHEMY_TRACK_MODIFICATION'] = False
+app.secret_key = 'apple' # to be confirmed
+api = Api(app)
+
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug=True)
