5ca5cd889fd607a3adabc6a4726635f1726fec1a
ShihWen <unhuman_7932@hotmail.com>
2022-08-02 16:16:29 +0800

    simplify file structure and adapt to sqlalchemy

--- app.py
+++ app.py
@@ -1,24 +1,54 @@
 from flask import Flask
 from flask_restful import Api
+from flask_restful import Resource
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import MetaData
+from sqlalchemy import Table, Column, Integer, String, DateTime
+from sqlalchemy import select
 
 from config import config
 # from db import db
-from resources.family_mart import FamilyMart
+# from resources.family_mart import FamilyMart
 
 params = config()
 DB_FULL_URL = f"postgresql+psycopg2://{params['user']}:{params['password']}@{params['host']}/{params['database']}"
 
 
 app = Flask(__name__)
-# app.config['SQL_ALCHEMY_DATABASE_URI'] = DB_FULL_URL
-# app.config['SQL_ALCHEMY_TRACK_MODIFICATION'] = False
-app.secret_key = 'apple' # to be confirmed
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_FULL_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'apple'
+db = SQLAlchemy(app)
+
 api = Api(app)
+
+metadata_obj = MetaData()
+fm_table = Table('cnvnt_str_fm', metadata_obj,
+    Column('extract_date', String),
+    Column('store_name', String),
+    Column('address', String)                
+)
+
+class FamilyMart(Resource):
+    def get(self, name):
+        results = db.session.query(fm_table).filter_by(store_name=name).all()
+        
+        output = {'store':[]}
+        
+        for row in results:
+            extract_date = str(row[0])
+            store_name = row[1]
+            address = row[2]
+            output['store'].append({'extract_date':extract_date,
+                'store_name':store_name, 
+                'address':address})
+        return output
+        
 
 
 api.add_resource(FamilyMart, '/familymart/<string:name>')
 
 
 if __name__ == '__main__':
-    # db.init_app(app)
     app.run(port=5000, debug=True)
---
+++ sqlalchemy_study.py
@@ -0,0 +1,31 @@
+# import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
+from sqlalchemy import Table, Column, Integer, String, DateTime
+from sqlalchemy import select
+
+from config import config
+# check version: 1.4.39
+# print(sqlalchemy.__version__)
+
+params = config()
+DB_FULL_URL = f"postgresql+psycopg2://{params['user']}:{params['password']}@{params['host']}/{params['database']}"
+
+
+engine = create_engine(DB_FULL_URL, echo=True)
+metadata_obj = MetaData()
+
+fm_table = Table('cnvnt_str_fm', metadata_obj,
+    Column('extract_date', DateTime),
+    Column('store_name', String),
+    Column('address', String)            
+)
+
+stmt = select(fm_table).where(fm_table.c.store_name == '全家新德光店')
+print(stmt)
+
+with engine.connect() as conn:
+    for row in conn.execute(stmt):
+        print(row)
+        break
+
