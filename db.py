df03bd5d018731e9d832edf50d22313d399c5549
ShihWen <unhuman_7932@hotmail.com>
2022-08-03 17:42:20 +0800

    Add columns to reflecting table,install simplejson

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy import Table, Column, Integer, String, DateTime
+from sqlalchemy import Table, Column, Integer, String, DateTime, Numeric
 
 db = SQLAlchemy()
 
@@ -9,5 +9,10 @@ metadata_obj = MetaData()
 fm_table = Table('cnvnt_str_fm', metadata_obj,
     Column('extract_date', String),
     Column('store_name', String),
-    Column('address', String)                
+    Column('city', String),
+    Column('district', String),
+    Column('address', String),
+    Column('longitude', Numeric),
+    Column('latitude', Numeric),
+    Column('services', String)                
 )
--- resources/family_mart.py
+++ resources/family_mart.py
@@ -1,4 +1,6 @@
+import simplejson as json
 from flask_restful import Resource
+
 
 from db import db
 from db import fm_table
@@ -6,8 +8,18 @@ from db import fm_table
 
 class FamilyMart(Resource):
     def get(self, name):
-        results = db.session.query(fm_table).filter_by(store_name=name).\
-            order_by(fm_table.c.extract_date.desc()).limit(3)
+
+        fields = (fm_table.c.extract_date, 
+                  fm_table.c.store_name, 
+                  fm_table.c.address, 
+                  fm_table.c.longitude, 
+                  fm_table.c.latitude,
+                  fm_table.c.services)
+
+        results = db.session.query(fm_table).\
+            with_entities(*fields).\
+            filter(fm_table.c.store_name==name).\
+            order_by(fm_table.c.extract_date.desc()).limit(1)
         
         output = {'store':[]}
         
@@ -15,8 +27,14 @@ class FamilyMart(Resource):
             extract_date = str(row[0])
             store_name = row[1]
             address = row[2]
+            lng = json.dumps(row[3], use_decimal=True)
+            lat = json.dumps(row[4], use_decimal=True)
+            services = row[5].split(",")
             output['store'].append({'extract_date':extract_date,
                 'store_name':store_name, 
-                'address':address})
+                'address':address,
+                'longitude':lng,
+                'latitude': lat,
+                'services':services})
         return output
         
