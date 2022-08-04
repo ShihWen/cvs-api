5a8bb5dc6a90d0c8da850cfdbe79e4e8d7fbdbf6
ShihWen <unhuman_7932@hotmail.com>
2022-08-04 16:05:07 +0800

    add 3 resources: get stores by city, cnt stores

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 
+
 from config import config
 from db import db
-from resources.family_mart import FamilyMart
+from resources.family_mart import FamilyMartByName
+from resources.family_mart import FamilyMartByCity
+from resources.family_mart import FamilyMartStoreNumByCity
+from resources.family_mart import FamilyMartStoreNumByGivenCity
 
 params = config()
 DB_FULL_URL = f"postgresql+psycopg2://{params['user']}:{params['password']}@{params['host']}/{params['database']}"
@@ -12,12 +16,17 @@ DB_FULL_URL = f"postgresql+psycopg2://{params['user']}:{params['password']}@{par
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_FULL_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 app.secret_key = 'apple'
 
 api = Api(app)
 
 
-api.add_resource(FamilyMart, '/familymart/<string:name>')
+api.add_resource(FamilyMartByName, '/fm_byname/<string:name>')
+api.add_resource(FamilyMartByCity, '/fm_bycity/<string:city>')
+api.add_resource(FamilyMartStoreNumByCity, '/fm_stores')
+api.add_resource(FamilyMartStoreNumByGivenCity,'/fm_stores/<string:city>')
+
 
 
 if __name__ == '__main__':
--- resources/family_mart.py
+++ resources/family_mart.py
@@ -1,40 +1,122 @@
 import simplejson as json
 from flask_restful import Resource
+from sqlalchemy import func
 
 
 from db import db
 from db import fm_table
 
 
-class FamilyMart(Resource):
+class Tools:
+    @classmethod
+    def get_latest_dt(cls): 
+        latest_dt = db.session.query(fm_table.c.extract_date).\
+        group_by(fm_table.c.extract_date).\
+        order_by(fm_table.c.extract_date.desc()).limit(1).cte('latest_dt')
+
+        latest_var = db.session.query(fm_table.c.extract_date).\
+        group_by(fm_table.c.extract_date).\
+        order_by(fm_table.c.extract_date.desc()).limit(1)
+
+        return latest_dt, str(latest_var[0][0])
+
+
+class FamilyMartByName(Resource):
     def get(self, name):
 
-        fields = (fm_table.c.extract_date, 
-                  fm_table.c.store_name, 
+        fields = (fm_table.c.store_name, 
                   fm_table.c.address, 
                   fm_table.c.longitude, 
-                  fm_table.c.latitude,
-                  fm_table.c.services)
+                  fm_table.c.latitude)
 
+        latest_cte, latest_var = Tools.get_latest_dt()
+        
         results = db.session.query(fm_table).\
             with_entities(*fields).\
-            filter(fm_table.c.store_name==name).\
-            order_by(fm_table.c.extract_date.desc()).limit(1)
+            filter(fm_table.c.store_name.like(f'%{name}%'), 
+                   fm_table.c.extract_date==latest_cte.c.extract_date)
         
-        output = {'store':[]}
+        output = {'extract_date':latest_var, 'store':[]}
         
         for row in results:
-            extract_date = str(row[0])
-            store_name = row[1]
-            address = row[2]
-            lng = json.dumps(row[3], use_decimal=True)
-            lat = json.dumps(row[4], use_decimal=True)
-            services = row[5].split(",")
-            output['store'].append({'extract_date':extract_date,
-                'store_name':store_name, 
+            
+            store_name = row[0]
+            address = row[1]
+            lng = json.dumps(row[2], use_decimal=True)
+            lat = json.dumps(row[3], use_decimal=True)
+            output['store'].append({'store_name':store_name, 
                 'address':address,
                 'longitude':lng,
-                'latitude': lat,
-                'services':services})
+                'latitude': lat})
+            
+        return output
+
+
+class FamilyMartByCity(Resource):
+    def get(self, city):
+
+        fields = (fm_table.c.store_name, 
+                  fm_table.c.address, 
+                  fm_table.c.longitude, 
+                  fm_table.c.latitude)
+
+        latest_cte, latest_var = Tools.get_latest_dt()
+        
+        refined_city = city.replace('臺', '台')
+        results = db.session.query(fm_table).\
+            with_entities(*fields).\
+            filter(fm_table.c.address.like(f'{refined_city}%'), 
+                   fm_table.c.extract_date==latest_cte.c.extract_date)
+        
+        output = {'extract_date':latest_var, 'store':[]}
+        
+        for row in results:
+            store_name = row[0]
+            address = row[1]
+            lng = json.dumps(row[2], use_decimal=True)
+            lat = json.dumps(row[3], use_decimal=True)
+            output['store'].append({'store_name':store_name, 
+                'address':address,
+                'longitude':lng,
+                'latitude': lat})
+            
+        return output
+
+
+class FamilyMartStoreNumByCity(Resource):
+    def get(self):
+
+        latest_cte, latest_var = Tools.get_latest_dt()
+        
+        results = db.session.query(fm_table.c.city, func.count(fm_table.c.store_name)).\
+            filter(fm_table.c.extract_date==latest_cte.c.extract_date).\
+            group_by(fm_table.c.city).\
+            order_by(func.count(fm_table.c.store_name).desc())
+        
+        output = {'extract_date':latest_var, 'store_counts':{}}
+
+        for city_name, store_count in results:
+            output['store_counts'][city_name] = store_count
+                
+        return output
+
+
+class FamilyMartStoreNumByGivenCity(Resource):
+    def get(self, city):
+
+        latest_cte, latest_var = Tools.get_latest_dt()
+        
+        refined_city = city.replace('臺', '台')
+        results = db.session.query(fm_table.c.city,fm_table.c.district, func.count(fm_table.c.store_name)).\
+            filter(fm_table.c.address.like(f'{refined_city}%'),
+                   fm_table.c.extract_date==latest_cte.c.extract_date).\
+            group_by(fm_table.c.city, fm_table.c.district).\
+            order_by(func.count(fm_table.c.store_name).desc())
+        
+        output = {'extract_date':latest_var, 'store_counts':{}}
+        for city_name, district_name, store_count in results:
+            city_district = city_name + district_name
+            output['store_counts'][city_district] = store_count
+                
         return output
         
